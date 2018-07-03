3ce13a20bd6b856df8b59ce0a391358a0ed79dc1
dean <39108638@qq.com>
2018-07-03 16:51:37 +0800

    修改sewage_info表结构

--- src/robodb/schema_sewage.go
+++ src/robodb/schema_sewage.go
@@ -1,23 +1,22 @@
 package robodb
 
-
 //sewage_info 表
 type SewageInfo struct {
-	ID                 int     `json:"-"`
-	SlnNo              string  `json:"sln_no"`
-	SewageBusiness     string  `json:"sewage_business"`
-	SewageScenario     string  `json:"sewage_scenario"`
-	TechMethod         string  `json:"tech_method"`
-	GeneralNorm        string  `json:"general_norm"`
-	OtherNorm          string  `json:"other_norm"`
-	DailyCapacity      float32 `json:"daily_capacity"`
-	Disinfector        int     `json:"disinfector"`
-	Valve              int     `json:"valve"`
-	Blower             int     `json:"blower"`
-	Stirrer            int     `json:"stirrer"`
-	AuxEquipmentNubs   string  `json:"aux_equipment_nubs"`
-	TotalEquipmentNubs string  `json:"total_equipment_nubs"`
-	Pump			   int     `json:"pump"`
-	Doser			   int     `json:"doser"`
-	OperatingSize	   string  `json:"operating_size"`
+	ID                 int    `json:"-"`
+	SlnNo              string `json:"sln_no"`
+	SewageBusiness     string `json:"sewage_business"`
+	SewageScenario     string `json:"sewage_scenario"`
+	TechMethod         string `json:"tech_method"`
+	GeneralNorm        string `json:"general_norm"`
+	OtherNorm          string `json:"other_norm"`
+	DailyCapacity      string `json:"daily_capacity"`
+	Disinfector        string `json:"disinfector"`
+	Valve              string `json:"valve"`
+	Blower             string `json:"blower"`
+	Stirrer            string `json:"stirrer"`
+	AuxEquipmentNubs   string `json:"aux_equipment_nubs"`
+	TotalEquipmentNubs string `json:"total_equipment_nubs"`
+	Pump               string `json:"pump"`
+	Doser              string `json:"doser"`
+	OperatingSize      string `json:"operating_size"`
 }
