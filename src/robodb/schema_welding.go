aeea9437fa2947a523083dfad16d5c40214c7125
dean <39108638@qq.com>
2018-07-10 10:32:53 +0800

    修改wedlinginfo字段

--- src/robodb/schema_welding.go
+++ src/robodb/schema_welding.go
@@ -1,25 +1,21 @@
 package robodb
 
-
 // welding_info 表
 type WeldingInfo struct {
-	ID                int     `json:"-"`
-	SlnNo             string  `json:"sln_no"`
-	WeldingBusiness   string  `json:"welding_business"` // todo
-	WeldingScenario   string  `json:"welding_scenario"` //todo
-	WeldingMetal      string  `json:"welding_metal"`
-	WeldingEfficiency string  `json:"welding_efficiency"`
-	WeldingSplash     string  `json:"welding_splash"`
-	WeldingModel      string  `json:"welding_model"`
-	WeldingMethod     string  `json:"welding_method"`
-	WeldingGas        string  `json:"welding_gas"`
-	GasCost           string  `json:"gas_cost"`
-	MaxHeight         float64 `json:"max_height"`
-	MaxRadius         float64 `json:"max_radius"`
+	ID                int    `json:"-"`
+	SlnNo             string `json:"sln_no"`
+	WeldingBusiness   string `json:"welding_business"` // todo
+	WeldingScenario   string `json:"welding_scenario"` //todo
+	WeldingMetal      string `json:"welding_metal"`
+	WeldingEfficiency string `json:"welding_efficiency"`
+	WeldingSplash     string `json:"welding_splash"`
+	WeldingModel      string `json:"welding_model"`
+	WeldingMethod     string `json:"welding_method"`
+	WeldingGas        string `json:"welding_gas"`
+	GasCost           string `json:"gas_cost"`
+	MaxHeight         string `json:"max_height"`
+	MaxRadius         string `json:"max_radius"`
 }
-
-
-
 
 // welding_tech_param 表
 type WeldingTechParam struct {
